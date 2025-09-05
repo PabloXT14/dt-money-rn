5068247a04146d38065e50ccc3a2389d058600fb
Pablo Alan <pabloxt14@gmail.com>
2025-09-05 15:34:09 -0300

    feat: create button component

---
+++ mobile/src/components/shared/button/index.tsx
@@ -0,0 +1,57 @@
+import {
+  Text,
+  TouchableOpacity,
+  type TouchableOpacityProps,
+} from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+import clsx from "clsx"
+
+import { colors } from "@/shared/colors"
+
+type ButtonMode = "fill" | "outline"
+
+type ButtonProps = TouchableOpacityProps & {
+  mode?: ButtonMode
+  iconName?: keyof typeof MaterialIcons.glyphMap
+}
+
+export const Button = ({
+  mode = "fill",
+  iconName,
+  children,
+  className,
+  ...rest
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      className={clsx(
+        "h-[57px] w-full flex-row items-center rounded-lg px-5",
+        iconName ? "justify-between" : "justify-center",
+        {
+          "bg-accent-brand": mode === "fill",
+          "border border-accent-brand bg-transparent": mode === "outline",
+        },
+        className
+      )}
+      {...rest}
+    >
+      <Text
+        className={clsx("text-base", {
+          "text-white": mode === "fill",
+          "text-accent-brand": mode === "outline",
+        })}
+      >
+        {children}
+      </Text>
+
+      {iconName && (
+        <MaterialIcons
+          name={iconName}
+          size={24}
+          color={mode === "fill" ? colors.white : colors["accent-brand"]}
+        />
+      )}
+    </TouchableOpacity>
+  )
+}
