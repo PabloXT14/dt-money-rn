4d0cd885a5ae0760c85aea26e925f5b355e122c8
Pablo Alan <pabloxt14@gmail.com>
2025-09-17 15:50:27 -0300

    feat: create header to home screen

--- mobile/src/app/(private)/index.tsx
+++ mobile/src/app/(private)/index.tsx
@@ -1,19 +1,10 @@
-import { Text, View } from "react-native"
-
-import { Button } from "@/components/shared/button"
-
-import { useAuthContext } from "@/contexts/auth.context"
+import { Header } from "@/components/screens/home/header"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 export default function Home() {
-  const { handleLogout } = useAuthContext()
-
   return (
-    <View className="flex-1 items-center justify-center bg-background-primary p-6">
-      <Text className="mb-4 font-roboto text-lg text-white">Home Screen</Text>
-
-      <Button iconName="logout" onPress={handleLogout}>
-        Sair
-      </Button>
-    </View>
+    <SafeAreaView className="flex-1 bg-background-primary">
+      <Header />
+    </SafeAreaView>
   )
 }
---
+++ mobile/src/components/screens/home/header/index.tsx
@@ -0,0 +1,35 @@
+import { Image, TouchableOpacity, View, Text } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+
+import { colors } from "@/shared/colors"
+
+import { useAuthContext } from "@/contexts/auth.context"
+
+export const Header = () => {
+  const { handleLogout } = useAuthContext()
+
+  return (
+    <View className="w-full flex-row items-center justify-between gap-2 p-8">
+      <View className="flex-1 gap-2">
+        <Image
+          source={require("@/assets/logo.png")}
+          alt="Logo"
+          className="h-[30px] w-[130px]"
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={handleLogout}
+          className="flex-row items-center gap-1"
+        >
+          <MaterialIcons name="logout" size={16} color={colors.gray[700]} />
+          <Text className="text-gray-700 text-xs">Sair da conta</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity className="h-[40px] w-[140px] items-center justify-center rounded-md bg-accent-brand">
+        <Text className="font-bold text-sm text-white">Nova transação</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
